33eb4cb10edeb0e73ed486dcb20cc86091794752
asterlion <astreiko1.22@gmail.com>
2023-12-03 16:26:53 +0300

    View list of contscts

---
+++ app/src/main/java/com/sapon/chat/activity/ChatNewActivity.kt
@@ -0,0 +1,19 @@
+package com.sapon.chat.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.sapon.chat.R
+
+class ChatNewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_new)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_chat)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/sapon/chat/data/Message.kt
@@ -0,0 +1,5 @@
+package com.sapon.chat.data
+
+import java.util.Date
+
+data class Message( val id: String, val from: String, val to: String, val text: String, val date: Date)
